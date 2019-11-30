f2e08eb72f3dc8b34e805713a32fd5301db63d0c
neytrino <neytrino070902@yandex.ru>
2019-11-30 06:37:56 +0300

    passed 5 part. added templates

--- foxass/catalog/views.py
+++ foxass/catalog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 # Create your views here.
-def hello(request):
-    return HttpResponse('це мерзость')
+def index(request):
+    num_books = Book.objects.all().count()  # количество записей в моделе Book
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()  # количество доступных книг. __exact означает точное совпадение с учетом регистра
+    num_authors = Author.objects.all().count()
+
+    return render(request, 'index.html', context={'num_books': num_books, 'num_instances': num_instances,
+                                                  'num_instances_available': num_instances_available, 'num_authors': num_authors})
